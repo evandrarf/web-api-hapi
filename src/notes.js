5dff693208c5010899828d558d56549c8ca28bb7
evandrarf <evandraraditya049@gmail.com>
2022-12-28 22:56:29 +0700

    Initial commit

---
+++ src/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    title: "babi",
+    tags: "hai",
+    body: "babi adalah lamu",
+    id: "vQJWL4lgJ3l_tLps",
+    createdAt: "2022-12-27T22:29:47.568Z",
+    updatedAt: "2022-12-27T22:29:47.568Z",
+  },
+  {
+    title: "Rudi",
+    tags: "mimikri",
+    body: "Alamak",
+    id: "t2SteNRxlDE2YuAU",
+    createdAt: "2022-12-27T22:35:25.172Z",
+    updatedAt: "2022-12-27T22:35:25.172Z",
+  },
+];
+
+module.exports = notes;
